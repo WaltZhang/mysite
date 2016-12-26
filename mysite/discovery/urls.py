cee56e4df30f552d84c1cb61ecab5ee6ca0108dc
WaltZhang <WaltZhang@users.noreply.github.com>
2016-12-26 16:47:12 +0800

    Add files via upload

--- mysite/discovery/urls.py
+++ mysite/discovery/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = 'discovery'
 urlpatterns = [
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login_user/$', views.login_user, name='login_user'),
+    url(r'^logout_user/$', views.logout_user, name='logout_user'),
     url(r'^$', views.projects, name='projects'),
     url(r'^project/(?P<pk>\d+)/$', views.ProjectView.as_view(), name='project'),
 ]
--- mysite/discovery/views.py
+++ mysite/discovery/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from django.db.models.query_utils import Q
+from django.contrib.auth import authenticate, login, logout
 from .models import Project, Dataset
+from .forms import UserForm
 
 
 class ProjectsView(generic.ListView):
@@ -32,8 +34,50 @@ def projects(request):
             return render(request, 'discovery/projects.html', {
                 'projects': project_list,
             })
-        # else:
-        #     return render(request, 'discovery/projects.html', {
-        #         'projects': projects,
-        #     })
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                project_list = Project.objects.filter(user=request.user)
+                return render(request, 'discovery/projects.html', {'projects': project_list})
+            else:
+                return render(request, 'discovery/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'discovery/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'discovery/login.html')
+
+
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                project_list = Project.objects.filter(user=request.user)
+                return render(request, 'discovery/index.html', {'projects': project_list})
+    context = {
+        "form": form,
+    }
+    return render(request, 'discovery/register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'discovery/login.html', context)
 
