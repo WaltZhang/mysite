bc05a19b54bc1b82cb26ba892fff1ea4f4e5a01f
WaltZhang <WaltZhang@users.noreply.github.com>
2017-01-04 23:25:46 +0800

    Add files via upload

---
+++ mysite/discovery/sparks.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from django.conf import settings
+from pyspark.sql.session import SparkSession
+
+
+class Spark:
+    def __init__(self, app_name='django-spark', master='local'):
+        spark_home = settings.SPARK_HOME if settings.SPARK_HOME else '/usr/local/spark'
+        os.environ['SPARK_HOME'] = spark_home
+        sys.path.insert(0, settings.PYTHONPATH if settings.PYTHONPATH else os.path.join(spark_home, 'python'))
+        sys.path.insert(0, settings.PY4J_DIR if settings.PY4J_DIR else os.path.join(spark_home,
+                                                                                    "python/lib/py4j-0.10.4-src"))
+        sys.path.insert(0, settings.PYSPARK_DIR if settings.PYSPARK_DIR else os.path.join(spark_home, 'python/pyspark'))
+        if settings.PYSPARK_PYTHON:
+            os.environ['PYSPARK_PYTHON'] = settings.PYSPARK_PYTHON
+        if settings.JAVA_HOME:
+            os.environ['JAVA_HOME'] = settings.JAVA_HOME
+        if settings.HADOOP_CONF_DIR:
+            os.environ['HADOOP_CONF_DIR'] = settings.HADOOP_CONF_DIR
+        self.spark = self.init_spark(app_name, master)
+
+    def init_spark(self, app_name, master):
+        return SparkSession\
+            .builder\
+            .appName(app_name)\
+            .enableHiveSupport()\
+            .master(master)\
+            .getOrCreate()
+        # .config("spark.sql.warehouse.dir", '/Users/walt/Development/python/mysite')\
+
