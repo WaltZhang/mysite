122b788246bf9f9f451277cbb08d741f3f6eef4c
WaltZhang <WaltZhang@users.noreply.github.com>
2017-01-04 23:21:39 +0800

    Add files via upload

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,10 +11,19 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Spark settings
+SPARK_HOME = '/usr/local/spark'
+PYTHONPATH = None
+PY4J_DIR = None
+PYSPARK_DIR = None
+PYSPARK_PYTHON = '/Users/walt/Development/anaconda3/bin/python3'
+JAVA_HOME = None
+HADOOP_CONF_DIR = None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -26,7 +35,6 @@ SECRET_KEY = '@ud=r#w!7zm#otn43qq#ngcj-wc(^%#cc*2cvwt__l^8_1#p$g'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -71,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -81,7 +88,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -101,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -115,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
