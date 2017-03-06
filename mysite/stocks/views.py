c51a65ea369920a9474e11ee4abec15e61380865
WaltZhang <WaltZhang@users.noreply.github.com>
2017-03-06 23:11:39 +0800

    Add files via upload

--- mysite/stocks/views.py
+++ mysite/stocks/views.py
@@ -10,24 +10,25 @@ class InvestmentView(ListView):
     context_object_name = 'investment_list'
 
     def get_queryset(self):
-        ivs = InvestmentModel.objects.filter(user__username=self.request.user.username)
-        return ivs
+        return InvestmentModel.objects.filter(user__username=self.request.user.username)
 
 
 class StockView(SingleObjectMixin, ListView):
     template_name = 'stocks/stock.html'
+    context_object_name = 'stock_list'
     paginate_by = 30
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object(queryset=InvestmentModel.objects.all())
-        return super(StockModel, self).get(request, *args, **kwargs)
+        return super(StockView, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        context = super(StockModel, self).get_context_data(**kwargs)
+        context = super(StockView, self).get_context_data(**kwargs)
         context['investment'] = self.object
+        return context
 
     def get_queryset(self):
-        return self.object.stock_set.all()
+        return self.object.stockmodel_set.all()
 
 
 class StockDetailView(DetailView):
