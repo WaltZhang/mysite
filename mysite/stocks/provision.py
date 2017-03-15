f3c595cab3deb58c3a64e63bbaf64b7766ded70b
WaltZhang <WaltZhang@users.noreply.github.com>
2017-03-16 07:15:59 +0800

    Add files via upload

--- mysite/stocks/provision.py
+++ mysite/stocks/provision.py
@@ -1,35 +1,48 @@
-from datetime import datetime
-from urllib import request, error
-from .models import StockModel
+from datetime import datetime, date
+from urllib import request
+from .models import StockModel, InvestmentModel
 
 
-class StockProvider(object):
-    tmp_file = '/tmp/table.csv'
+def create_investment(code, user):
+    investment = InvestmentModel.objects.create(code=code, user=user)
+    load_latest_data(investment=investment, code=code)
+    return investment
+
+
+def _get_stock_csv(code, tmp_file):
     stock_url = 'http://table.finance.yahoo.com/table.csv?s={}'
+    url = stock_url.format(code)
+    request.urlretrieve(url, tmp_file)
 
-    def __init__(self, stock_code, start_date='1990-12-01'):
-        self.stock_code = stock_code
-        self.start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
 
-    def _get_stock_csv(self):
-        url = self.stock_url.format(self.stock_code)
-        request.urlretrieve(url, self.tmp_file)
-
-    def load_latest_data(self):
-        self._get_stock_csv()
-        with open(self.tmp_file) as csv:
-            next(csv)
-            for row in csv:
-                columns = row.split(',')
-                column_date = datetime.strptime(columns[0], '%Y-%m-%d').date()
-                if column_date > self.start_date:
-                    stock = StockModel()
-                    stock.code = self.stock_code
-                    stock.date = column_date
-                    stock.open = float(columns[1])
-                    stock.high = float(columns[2])
-                    stock.low = float(columns[3])
-                    stock.close = float(columns[4])
-                    stock.volume = int(columns[5])
-                    stock.adj_close = float(columns[6])
-                    stock.save()
+def load_latest_data(investment, code, start_date=date(1990, 12, 1)):
+    tmp_file = '/tmp/table.csv'
+    _get_stock_csv(code=code, tmp_file=tmp_file)
+    with open(tmp_file) as csv:
+        next(csv)
+        for row in csv:
+            columns = row.split(',')
+            column_date = datetime.strptime(columns[0], '%Y-%m-%d').date()
+            if column_date > start_date:
+                investment.stockmodel_set.create(
+                    investment=investment,
+                    code=code,
+                    date=column_date,
+                    open=float(columns[1]),
+                    high=float(columns[2]),
+                    low=float(columns[3]),
+                    close=float(columns[4]),
+                    volume=int(columns[5]),
+                    adj_close=float(columns[6])
+                )
+                # stock = StockModel()
+                # stock.investment = investment
+                # stock.code = code
+                # stock.date = column_date
+                # stock.open = float(columns[1])
+                # stock.high = float(columns[2])
+                # stock.low = float(columns[3])
+                # stock.close = float(columns[4])
+                # stock.volume = int(columns[5])
+                # stock.adj_close = float(columns[6])
+                # stock.save()
--- mysite/stocks/views.py
+++ mysite/stocks/views.py
@@ -1,8 +1,10 @@
 from django.views.generic.detail import SingleObjectMixin
-from django.views.generic import ListView, DetailView
-from datetime import datetime
-from .models import StockModel, InvestmentModel
-from .provision import StockProvider
+from django.views.generic import ListView, TemplateView
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from .models import InvestmentModel, StockModel
+from .provision import create_investment
 
 
 class InvestmentView(ListView):
@@ -11,6 +13,16 @@ class InvestmentView(ListView):
 
     def get_queryset(self):
         return InvestmentModel.objects.filter(user__username=self.request.user.username)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            count = StockModel.objects.filter(code=request.POST.get('stock_code')).count()
+            if count == 0:
+
+            investment = InvestmentModel.objects.get(code=request.POST.get('stock_code'))
+        except ObjectDoesNotExist:
+            investment = create_investment(code=request.POST.get('stock_code'), user=self.request.user)
+        return HttpResponse(investment)
 
 
 class StockView(SingleObjectMixin, ListView):
@@ -30,7 +42,3 @@ class StockView(SingleObjectMixin, ListView):
     def get_queryset(self):
         return self.object.stockmodel_set.all()
 
-
-class StockDetailView(DetailView):
-    template_name = 'stocks/detail.html'
-    model = StockModel
