93b77f2c8b1e50db0599ab07119e96721c48e014
WaltZhang <WaltZhang@users.noreply.github.com>
2017-03-22 23:07:57 +0800

    Add files via upload

--- mysite/stocks/urls.py
+++ mysite/stocks/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from stocks.views import StockView
+from . import views
+
+app_name = 'stocks'
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', StockView.as_view(), name='stocks'),
+    url(r'^$', views.InvestmentView.as_view(), name='investments'),
+    url(r'^(?P<pk>\d+)/$', views.StockView.as_view(), name='stock'),
+    url(r'^(?P<pk>\d+)/delete/$', views.InvestmentDelete.as_view(), name='delete'),
 ]
